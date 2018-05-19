a39c1fc0d615aa5144717eee46d178855062eaa8
Kevin McCartney <hello@kevinmccartney.is>
2018-05-18 23:32:41 -0400

    Added error handling for everything except a GET

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/*', (req, res) => res.send('Hello World!'));
+
+// lock down all other methods for all other routes
+app.all('*', (req, res) => res.status(405).send({
+  message: 'Method not allowed',
+  code: 405
+}));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
